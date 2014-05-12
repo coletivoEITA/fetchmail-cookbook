585f189d7feebbc3d8a4f281c888b06879fc87b4
Braulio Bhavamitra <braulio@eita.org.br>
2014-05-12 01:49:58 +0000

    Update version

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'braulio@eita.org.br'
 license          'GPLv3+'
 description      'Installs/Configures Fetchmail'
 long_description File.read("#{File.dirname __FILE__}/README.md")
-version          '1.0.0'
+version          '1.0.1'
 
 
