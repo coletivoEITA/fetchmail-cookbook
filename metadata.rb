e1c72fd42446b692df12e8b85f5668f4e52331ea
Braulio Bhavamitra <braulio@eita.org.br>
2014-10-02 17:26:51 -0300

    Replace utf8 char

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
 name             'fetchmail'
-maintainer       'Bráulio Bhavamitra'
+maintainer       'Braulio Bhavamitra'
 maintainer_email 'braulio@eita.org.br'
 license          'GPLv3+'
 description      'Installs/Configures Fetchmail'
